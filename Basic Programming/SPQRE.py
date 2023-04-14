829b4f241c7caa741211ae93975d2e28c2fd1fb9
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-14 21:57:23 +0530

    Done

---
+++ Basic Programming/SPQRE.py
@@ -0,0 +1,17 @@
+# Input two numbers and find their sum, product, difference, quotient, remainder, exponent
+import math
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+sum=a+b
+prod=a*b
+diff=a-b
+quo=a/b
+rem=a%b
+exp=math.pow(a,b)
+print(sum)
+print(prod)
+print(diff)
+print(quo)
+print(rem)
+print(exp)
+
