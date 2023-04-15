f8fb9e08f0d3f6752b7158339ce9557d77c75b61
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-15 11:44:37 +0530

    Done

---
+++ Basic Programming/Circle.py
@@ -0,0 +1,9 @@
+# Input radius and find area and circumference of a circle and a semi-circle
+
+r=int(input("Enter Radius"))
+A=22.0/7*r**2
+C=2*22.0/7*r
+a=(22.0/7*r*r)/2
+c=(22.0/7*r)
+print(A,C)
+print(a,c)
