dbdbdd16807c4892521167fc15116294b8506ae0
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-17 02:25:18 +0530

    Done

---
+++ Conditional Statements/ThreeNumberGreater.py
@@ -0,0 +1,11 @@
+# Input three numbers and find the greater number
+
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
