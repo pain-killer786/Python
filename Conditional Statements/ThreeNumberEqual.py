946cfd1256d9502304fd853e4625e941ef5ed6e5
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-17 02:30:51 +0530

    Done

---
+++ Conditional Statements/ThreeNumberEqual.py
@@ -0,0 +1,8 @@
+# Input three numbers and find the greater number  
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if a==b==c:
+    print("Equal")
+else:
+    print("Not")
