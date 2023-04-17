52957ab30d9a414776050f9c12a8bd08a9c6c21d
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-17 12:46:53 +0530

    Done

---
+++ Conditional Statements/PositiveNegativeZero.py
@@ -0,0 +1,11 @@
+# Input a number and find if it is positive, negative or zero
+
+a=int(input("Enter a number"))
+if a>0:
+    print("Positve")
+elif a<0:
+    print("Negative")
+else:
+    print("Zero")
+    
+    
