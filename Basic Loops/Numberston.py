25672b0dde37e8692b1be151514dca159858dd1d
Krishnendu Nandy <krishnendunandy43@gmail.com>
2023-04-18 19:10:42 +0530

    Done

---
+++ Basic Loops/Numberston.py
@@ -0,0 +1,12 @@
+# Input a number and print all the numbers upto that number and from that number to 1
+
+n=int(input("Enter a number"))
+i=1
+while(i<=n): #Loop to print in Forward Direction
+    print(i)
+    i+=1
+    
+i=n
+while(i>=1): #Loop to print in Backward Direction
+    print(i)
+    i-=1
